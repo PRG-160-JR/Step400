456cadc59ac4f0003d45f77648fb8032495c1413
jrissler9 <jrissler9@gmail.com>
2018-02-16 16:28:17 -0500

    added an animated header, tagline, main and footer through app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,4 +10,37 @@
 
       document.title = applicationTitle;
 
+      document.body.style.backgroundColor = '#BBB';
+      document.body.style.textAlign = 'center';
+      document.body.style.fontSize = '3em';
+
+      var elWrapper = document.createElement('div');
+      elWrapper.id = 'applicationWrapper';
+      document.body.appendChild(elWrapper);
+
+      var elHeader = document.createElement('header');
+      elWrapper.appendChild(elHeader);
+
+      var elHeaderTitle = document.createElement('h1');
+      elHeaderTitle.innerHTML = applicationTitle;
+      elHeader.appendChild(elHeaderTitle);
+
+      var elHeaderTagline = document.createElement('h2');
+      elHeaderTagline.textContent = applicationTagline;
+      elHeader.appendChild(elHeaderTagline);
+
+      elHeaderTagline.style.fontSize = '0.4em';
+
+      var elMain = document.createElement('main');
+      elMain.innerHTML = '<p>Hello World!</p>';
+      elWrapper.appendChild(elMain);
+
+      var elFooter = document.createElement('footer');
+      elFooter.innerHTML = '<h4>get excited about learning Javascript...';
+      elWrapper.appendChild(elFooter);
+
+      elHeader.className = 'animated bounceInDown';
+      elHeaderTagline.className = 'animated bounceInLeft';
+      elMain.className = 'animated zoomIn';
+      elFooter.className = 'animated bounceInRight';
   }
