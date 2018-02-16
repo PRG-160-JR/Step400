9f5aa4d7c884f4e46a71c59bf4712022c07692be
jrissler9 <jrissler9@gmail.com>
2018-02-16 16:01:38 -0500

    added a title to the document with js Step400

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+  'use strict';
+
+  var applicationTitle = 'inside out - js';
+
+  var applicationTagline = 'continuously falling foward into the light...';
+
+  window.addEventListener('load', initializeApplication);
+
+  function initializeApplication() {
+
+      document.title = applicationTitle;
+
+  }
